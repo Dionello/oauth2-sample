85aa3280e1e46fb24e9211b190fc53e0208abe05
Caio Cesar Dionello <ccdionello@gmail.com>
2023-04-18 23:11:12 -0300

    initial commit

---
+++ src/test/java/br/edu/ufabc/aluno/oauth2/Oauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufabc.aluno.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
